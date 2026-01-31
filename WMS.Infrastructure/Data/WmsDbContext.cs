4cb3ca228c5b2c89c84726f7a3de99f51088f3b4
vivekThoke <vivekthoke5@gmail.com>
2026-01-31 13:00:35 +0530

    added Entity core dbContext to infrastructure

---
+++ WMS.Infrastructure/Data/WmsDbContext.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WMS.Domain.Entities;
+
+namespace WMS.Infrastructure.Data
+{
+    public class WmsDbContext : DbContext
+    {
+        public WmsDbContext(DbContextOptions<WmsDbContext> options) : base(options) { }
+
+        public DbSet<WorkRequest> WorkRequests => Set<WorkRequest>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<WorkRequest>(entity =>
+            {
+                entity.HasKey(x => x.Id);
+
+                entity.Property(x => x.Title)
+                .IsRequired()
+                .HasMaxLength(200);
+            });
+
+            
+        }
+    }
+}
