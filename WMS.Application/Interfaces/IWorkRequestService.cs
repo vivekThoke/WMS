c599dc5f033f8c024e3880ac31e0c6e8c752ab31
vivekThoke <vivekthoke5@gmail.com>
2026-02-01 19:41:45 +0530

    Add inteface for request service

---
+++ WMS.Application/Interfaces/IWorkRequestService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WMS.Domain.Entities;
+
+namespace WMS.Application.Interfaces
+{
+    public interface IWorkRequestService
+    {
+        Task<WorkRequest> CreateAsync(WorkRequest request);
+        Task<IEnumerable<WorkRequest>> GetAllAsync();
+    }
+}
