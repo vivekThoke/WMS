18fda707993cc220f1753a9381e9ea230293de8a
vivekThoke <vivekthoke5@gmail.com>
2026-02-04 21:16:37 +0530

    adding global exception handler

---
+++ WMS.API/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,14 @@
+﻿namespace WMS.API.Middleware
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionMiddleware> _logger;
+
+        public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+    }
+}
