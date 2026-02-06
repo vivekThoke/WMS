5fe1cb1b90cd43f56985e8685ad12e3d2274a286
vivekThoke <vivekthoke5@gmail.com>
2026-02-06 22:33:57 +0530

    Registered Auth and Autherization in API

--- WMS.API/Program.cs
+++ WMS.API/Program.cs
@@ -1,13 +1,19 @@
 using FluentValidation;
 using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using WMS.API.Middleware;
 using WMS.Application.DTOs;
 using WMS.Application.Interfaces;
 using WMS.Application.Validators;
 using WMS.Infrastructure.Data;
+using WMS.Infrastructure.Identity;
 using WMS.Infrastructure.Services;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -22,6 +28,29 @@ builder.Services.AddDbContext<WmsDbContext>(options =>
     );
     
 });
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+    .AddEntityFrameworkStores<WmsDbContext>()
+    .AddDefaultTokenProviders();
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]!)
+            )
+        };
+    });
+
+
 builder.Services.AddScoped<IWorkRequestService, WorkRequestService>();
 
 builder.Services.AddFluentValidationAutoValidation();
