a8d40587e41f30cee15477b71fcacc50931d204b
vivekThoke <vivekthoke5@gmail.com>
2026-02-07 18:32:43 +0530

    Auth end point controller

---
+++ WMS.API/Controllers/AuthController.cs
@@ -0,0 +1,51 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualBasic;
+using WMS.Infrastructure.Identity;
+
+namespace WMS.API.Controllers
+{
+    [ApiController]
+    [Route("api/auth")]
+    public class AuthController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IConfiguration _configuration;
+
+        public AuthController(UserManager<ApplicationUser> userManager, IConfiguration configuration)
+        {
+            _userManager = userManager;
+            _configuration = configuration;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(string email, string password)
+        {
+            var user = new ApplicationUser { UserName = email, Email = email };
+            var result =  await _userManager.CreateAsync(user, password);
+
+            if (!result.Succeeded) return BadRequest(result.Errors);
+
+            return Ok();
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(string email, string password)
+        {
+           var user = await _userManager.FindByEmailAsync(email);
+
+            if (user == null || !await _userManager.CheckPasswordAsync(user, password))
+            {
+                return Unauthorized();
+            }
+
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.Name, user.Email)
+            }; 
+
+
+        }
+    }
+}
