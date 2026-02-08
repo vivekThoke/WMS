d566d39b329bfc8305b967b5f183949bd79e0636
vivekThoke <vivekthoke5@gmail.com>
2026-02-08 12:40:10 +0530

    Indentity role seeder with different roles access

---
+++ WMS.Infrastructure/Identity/IdentitySeeder.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.VisualBasic;
+
+namespace WMS.Infrastructure.Identity
+{
+    public static class IdentitySeeder
+    {
+        public static async Task SeedRolesAsync(RoleManager<IdentityRole> roleManager)
+        {
+            string[] roles = { "Employee", "Manager", "Admin" };
+
+            foreach(var role in roles)
+            {
+                if (!await roleManager.RoleExistsAsync(role))
+                {
+                    await roleManager.CreateAsync(new IdentityRole(role));
+                }
+            }
+        }
+    }
+}
